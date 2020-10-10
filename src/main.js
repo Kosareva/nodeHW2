af77903bf103f2c8fa0f6124643dcbc6a1b5132c
Yelyzaveta_Kosarieva <yelyzaveta.kosarieva@refinitiv.com>
2020-10-10 10:23:28 +0300

    chore: create database seed script

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 const startApp = require('./app');
-const config = require('./config');
-const environment = config.NODE_ENV || 'development';
-const seeds = require('./sequelize.config')[environment].custom.seeds;
 
 (async () => {
-  for (const seedsPath of seeds) {
-    await require(seedsPath)();
-  }
-  console.log('Seeds have been created');
   await startApp();
 })();
